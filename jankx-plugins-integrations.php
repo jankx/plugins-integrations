2d7eb86bf52e34093c56a342992af89e888b4e8e
Puleeno Nguyen <puleeno@gmail.com>
2020-09-23 03:22:48 +0700

    Update autoload files

---
+++ jankx-plugins-integrations.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Jankx Plugin Integrations
+ * Plugin URI: https://jankx.puleeno.com
+ * Author: Puleeno Nguyen
+ * Author URI: https://puleeno.com
+ * Description:  Make compatible with other premium WordPress plugins
+ * Version:1.0.0
+ */
+
---
+++ src/PluginIntegrationManager.php
@@ -0,0 +1,6 @@
+<?php
+namespace Jankx\Plugin\Integration;
+
+class PluginIntegrationManager
+{
+}
