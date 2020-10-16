d7f84f335712691a5b8fccf58d1a7165cc58fe92
Puleeno Nguyen <puleeno@gmail.com>
2020-10-16 07:18:46 +0700

    Fix assets plugin path on Windows

--- functions.php
+++ functions.php
@@ -1,9 +1,17 @@
 <?php
 function jankx_plugins_integration_asset_url($path = '') {
+    $abspath = constant('ABSPATH');
+    $pluginPath = dirname(JANKX_PLUGIN_INTEGRATION_BUNDLES_LOADER);
+
+    if (PHP_OS === 'WINNT') {
+        $abspath = str_replace('\\', '/', $abspath);
+        $pluginPath = str_replace('\\', '/', $pluginPath);
+    }
+
     $assetDirUrl = str_replace(
-        ABSPATH,
+        $abspath,
         site_url('/'),
-        dirname(JANKX_PLUGIN_INTEGRATION_BUNDLES_LOADER)
+        $pluginPath
     );
     return sprintf('%s/assets/%s', $assetDirUrl, $path);
 }
