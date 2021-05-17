d370206a78fb4d7dcf86c4057cdd2c70f31f9ce7
Puleeno Nguyen <puleeno@gmail.com>
2021-05-17 19:38:15 +0700

    Follow new structure of template engine

--- src/IntegrateTemplate.php
+++ src/IntegrateTemplate.php
@@ -1,30 +1,35 @@
 <?php
 namespace Jankx\PluginsIntegrations;
 
+use Jankx;
 use Jankx\Template\Template;
 
 class IntegrateTemplate
 {
-    protected static $templateLoader;
+    const ENGINE_ID = 'plugins_integrations';
 
-    public static function getLoader()
+    protected static $engine;
+
+    public static function getEngine()
     {
-        $defaultTemplateDir = sprintf('%s/templates', dirname(JANKX_PLUGIN_INTEGRATION_BUNDLES_LOADER));
-        if (is_null(static::$templateLoader)) {
-            static::$templateLoader = Template::getLoader(
-                $defaultTemplateDir,
-                apply_filters('jankx_plugin_integrations_template_directory', 'plugins'),
-                'wordpress'
+        if (is_null(static::$engine)) {
+            $engine = Template::createEngine(
+                static::ENGINE_ID,
+                apply_filters('jankx_plugin_integrations_template_directory', 'templates/plugins'),
+                sprintf('%s/templates', dirname(JANKX_PLUGIN_INTEGRATION_BUNDLES_LOADER)),
+                Jankx::getActiveTemplateEngine()
             );
+
+            static::$engine = &$engine;
         }
-        return static::$templateLoader;
+        return static::$engine;
     }
 
     public static function render()
     {
         $args = func_get_args();
         return call_user_func_array(
-            array(static::getLoader(), 'render'),
+            array(static::getEngine(), 'render'),
             $args
         );
     }
